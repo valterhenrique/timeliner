b637059320e60616e07da4dd498b381dd152c62b
Valter Silva <valter.silva@anevis-solutions.com>
2017-06-02 12:42:41 +0200

    Fetch raw data and sort it by date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
   require 'httparty'
   def index
     response = HTTParty.get('https://raw.githubusercontent.com/valterhenrique/timeliner-sample/master/sample-data.json')
-    @events = JSON.parse(response.body)
-    puts @events.to_json
+
+    @raw_data = JSON.parse(response.body)
+    @sorted_data = @raw_data.sort_by { |s| DateTime.parse(s["date"]) }
+    puts @sorted_data
   end
 
   def contact
